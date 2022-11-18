de7291f92a37ab73160b18cc6c655792cd210a31
lucasnkz11 <lucasnkz11@gmail.com>
2022-11-18 17:19:57 -0300

    getting started

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,11 @@
+import { IsString, MinLength, Matches } from 'class-validator';
+export class CreateUserDto {
+  @IsString()
+  @MinLength(4)
+  name: string;
+
+  @IsString()
+  @MinLength(8)
+  @Matches(/[0-9a-zA-Z]{8,}/)
+  password: string;
+}
